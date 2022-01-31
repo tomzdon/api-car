9fd47d615afc98b5569fd0ed00e8a3676968bc69
SPYRO\twl <twl@spyro-soft.com>
2022-01-31 17:33:51 +0100

    mlkhkl;

---
+++ src/api.js
@@ -0,0 +1,40 @@
+const cors = require("cors");
+const express = require("express");
+const fs = require("fs");
+const app = express();
+const serverless = require("serverless-http");
+const router = express.Router();
+
+global.__basedir = __dirname;
+
+var corsOptions = {
+  origin: "*",
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+router.post("/:name", (req, res) => {
+  const fileName = req.params.name;
+  const aut = req.body.aut;
+  const directoryPath = __basedir + "/.netlify/functions/resources/static/assets/uploads/";
+  if (aut != "codesample") {
+    res.status(401).send({
+      message: "Unatorized",
+    });
+  } else {
+    res.download(directoryPath + fileName, fileName, (err) => {
+      if (err) {
+        res.status(500).send({
+          message: "Could not download the file. " + err,
+        });
+      }
+    });
+  }
+});
+
+app.use(`/.netlify/functions/api`, router);
+
+module.exports = app;
+module.exports.handler = serverless(app);
